76722fcf9ad6e3922e7c6b05ac74a587446e8dd9
moneshs <moneshsundar19@gmail.com>
2020-02-06 16:57:14 +0530

    addednumguessgame

---
+++ num_guess_game.py
@@ -0,0 +1,18 @@
+import random
+winning_number = random.randint(1,100)
+n=int(input("enter a number btwn 1 and 100"))
+game_over=False
+guess=1
+while not game_over:
+    if n==winning_number:
+        print(f"you win,and u win in {guess}   guess")
+        game_over=True
+    else:
+        if winning_number>n:
+            print("too low")
+            guess+=1
+            n=int(input('input again'))
+        else:
+            print('too high')
+            guess+=1
+            n=int(input('input again'))
